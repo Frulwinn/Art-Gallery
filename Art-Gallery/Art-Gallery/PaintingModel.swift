c89b44248eb250b1bc49a9729d176b573647aa96
Frulwinn <frulwinn@gmail.com>
2018-11-11 15:02:25 -0800

    added images to assets folder

--- Art-Gallery/Art-Gallery/PaintingModel.swift
+++ Art-Gallery/Art-Gallery/PaintingModel.swift
@@ -3,15 +3,14 @@ import UIKit
 class PaintingModel: NSObject, UITableViewDataSource, PaintingTableViewCellDelegate {
    
     weak var PaintingViewController : PaintingViewController?
-    
     var paintings: [Painting] = []
     
     override init() {
         for images in 1 ... 12 {
-            let imageName = "image\(paintings)"
-            guard let image = UIImage(name: imageName) else {return}
-                let painting = Painting(image: image)
-                paintings.append(painting)
+            let imageName = "Image\(paintings)"
+            guard let image = UIImage(named: imageString) else {return}
+            let painting = Painting(image: image)
+            paintings.append(painting)
         }
     }
     func tappedLikeButton(on cell: PaintingTableViewCell) {
